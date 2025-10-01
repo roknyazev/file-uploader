ce5c9d1b9de8cdb316fad752d73089e5d1566eed
romak <romaknyazev55@yandex.ru>
2025-10-01 22:48:34 +0300

    Отключение tanstack router devtools и перемещение тостов в левый нижний угол

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,4 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanstackDevtools } from '@tanstack/react-devtools'
 import { Toaster } from '@/shared/components/ui/sonner'
 
 export const Route = createRootRoute({
@@ -9,18 +7,7 @@ export const Route = createRootRoute({
       <main className={'max-w-7xl mx-auto h-screen overflow-hidden'}>
         <Outlet />
       </main>
-      <TanstackDevtools
-        config={{
-          position: 'bottom-left',
-        }}
-        plugins={[
-          {
-            name: 'Tanstack Router',
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-        ]}
-      />
-      <Toaster position={'bottom-right'} closeButton richColors />
+      <Toaster position={'bottom-left'} closeButton richColors />
     </>
   ),
 })
